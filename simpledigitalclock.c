8ce1688edee050cb86c835e91fb9b00afe66addf
jespeste <jespeste@stud.ntnu.no>
2024-02-20 21:10:26 +0100

    Created a simple clock.

---
+++ simpledigitalclock.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main(int argc, char const *argv[])
+{
+    int h,m,s;
+    int d=100;
+    printf("Set time : \n");
+    scanf("%d%d%d",&h,&m,&s);
+    if(h>12 || m>60 || s>60)
+    {
+        printf("ERROR!\n");
+        exit(0);
+    }
+    while(1)
+    {
+        s++;
+        if(s>59)
+        {
+            m++;
+            s=0;
+        }
+        if(m>59)
+        {
+            h++;
+            m=0;
+        }
+        if(h>12)
+        {
+            h=1;
+        }
+        printf("\n Clock:");
+        printf("\n %02d:%02d:%02d",h,m,s);
+        Sleep(d); // The actual sleepy eepy boi
+        system("cls"); // This is a clear screen function, that clears the screen. Pretty cool. I've never seen it in use.
+        
+    }
+    return 0;
+}
